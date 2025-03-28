71fc65a4508cb4a41e965e485e095066deaa3366
Manuela <manuela.almada009@gmail.com>
2025-03-28 12:05:15 -0300

    Implementación DFS

--- tp-pathfinding/src/pathfinder/search/dfs.py
+++ tp-pathfinding/src/pathfinder/search/dfs.py
@@ -21,7 +21,45 @@ class DepthFirstSearch:
         # Initialize the explored dictionary to be empty
         explored = {} 
         
-        # Add the node to the explored dictionary
-        explored[node.state] = True
-        
-        return NoSolution(explored)
+        #Test-Objetivo
+        if node.state == grid.end:
+            return Solution(node, explored)
+            
+        frontier = StackFrontier()
+        frontier.add(node)
+
+        while True:
+
+            #  Fail if the frontier is empty
+            if frontier.is_empty():
+                return NoSolution(explored)
+            # Remove a node from the frontier
+            node = frontier.remove()
+            
+
+            if node.state in explored:
+                continue
+            explored[node.state]=True
+            successors = grid.get_neighbours(node.state)
+
+            for action, result in successors.items():  
+                new_state = result  # Obtener el nuevo estado
+
+
+                # Check if the successor is not reached
+                if new_state not in explored:
+
+                        # Initialize the son node
+                    new_node = Node("", new_state,
+                                        node.cost + grid.get_cost(new_state),
+                                        parent=node,action=action)
+                        # Mark the successor as reached
+                    #explored[new_state] = True
+
+                    # Return if the node contains a goal state
+                    if new_state == grid.end:
+                        return Solution(new_node, explored)
+                
+                  
+                    # Add the new node to the frontier
+                    frontier.add(new_node)
